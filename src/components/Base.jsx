b4f98dc5c3979dcafac79da65f0da6d42034bb07
brunocaike <brunokf1512@gmail.com>
2023-03-23 20:54:07 -0300

    feito o footer

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,10 +1,12 @@
 import '../assets/css/app.css';
 import Header from './Header';
+import Footer from './Footer';
 
 const Base = ({ children }) => (
     <>
         <Header />
         {children}
+        <Footer />
     </>
 );
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-orange-400">
+            <div className="container flex items-center h-24">
+                <p className="text-white text-xs">©2023 - Todos os direitos reservados</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
