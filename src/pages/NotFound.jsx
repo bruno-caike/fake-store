bf27d0cc5e6c524d0eeb3593e176b0a2d81c75cc
brunocaike <brunokf1512@gmail.com>
2023-03-25 18:45:17 -0300

    finalizado o layout da tela de erro 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,13 +4,13 @@ import { routes } from '../utils/variables';
 
 const NotFound = () =>  (
     <Base>
-        <section>
-            <div>
-                <header>
-                    <h1>Página não encontrada</h1>
-                    <p>A URL solicitada não pôde ser encontrada. <br />Por favor verifique o endereço ou informe o administrador.</p>
+        <section className='py-16'>
+            <div className="container">
+                <header className='text-center'>
+                    <h1 className='font-bold text-2xl sm:text-5xl mb-6'>Página não encontrada</h1>
+                    <p className='text-base sm:text-xl'>A URL solicitada não pôde ser encontrada. <br />Por favor verifique o endereço ou informe o administrador.</p>
                 </header>
-                <Link to={routes.home}>Voltar para início</Link>
+                <Link to={routes.home} className="mx-auto mt-5 sm:mt-10 flex items-center justify-center w-full sm:w-80 py-2 px-3 border border-slate-600 bg-slate-600 text-white rounded font-bold transition-all hover:text-slate-600 hover:bg-white">Voltar para início</Link>
             </div>
         </section>
         
